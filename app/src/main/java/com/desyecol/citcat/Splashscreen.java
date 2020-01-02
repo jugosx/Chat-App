fbffea642e50b0d2cc4cecfa8fc44f95f5572b08
jugosx <priskillacantix@gmail.com>
2020-01-02 18:00:59 +0700

    Create Fragment, Login, Register, Logout

---
+++ app/src/main/java/com/desyecol/citcat/Splashscreen.java
@@ -0,0 +1,16 @@
+package com.desyecol.citcat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
